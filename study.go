b5544acaa2f4809b8043e8d38739193128f07e99
kurarion <sirokuma@irisu.cc>
2019-08-10 00:56:42 +0800

    项目迁移至goland

--- study.go
+++ study.go
@@ -2,13 +2,17 @@ package main
 
 //注意同一个目录下不能有不同名的包
 import (
-	chapter4 "./Chapter4"
-	helloworld "./HelloWorld"
+	"fmt"
+	ch4 "./Chapter4"
+	helloworld	"./HelloWorld"
 )
 
 func main() {
 	//C4
-	chapter4.TestBase()
+	ch4.TestBase()
+
 	//
 	helloworld.Hello()
+
+	fmt.Println("xxx")
 }
